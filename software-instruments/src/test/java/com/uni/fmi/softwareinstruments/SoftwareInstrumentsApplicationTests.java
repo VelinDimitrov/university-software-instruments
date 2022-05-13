643698f65f507dbcaf482f714e5747ecf5aa4944
Velin Dimitrov <Velin.Dimitrov.ext@hoffmann-group.com>
2022-05-13 21:46:17 +0300

    initial spring project commit

---
+++ software-instruments/src/test/java/com/uni/fmi/softwareinstruments/SoftwareInstrumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.fmi.softwareinstruments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareInstrumentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
